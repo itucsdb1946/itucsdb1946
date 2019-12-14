5e05497ff09527b5a306c283fe55260709ce3e0b
CenkEralp <57072492+CenkEralp@users.noreply.github.com>
2019-12-14 22:52:36 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -21,6 +21,6 @@ def initialize(url):
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
     if url is None:
-        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
+        print("Usage: DATABASE_URL=https://itucsdb1946.herokuapp.com/ python dbinit.py", file=sys.stderr)
         sys.exit(1)
     initialize(url)
