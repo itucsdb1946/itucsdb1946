55343b68f54a6a26ee4c04628391b754c3e28429
CenkEralp <57072492+CenkEralp@users.noreply.github.com>
2019-12-14 23:19:38 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -5,8 +5,44 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """CREATE TABLE SITEUSER(
+                        ID SERIAL PRIMARY KEY,
+                        USERNAME VARCHAR(40),
+                        PASSWORD VARCHAR(100),
+                        USERTYPE VARCHAR(10));
+    """,
+    """
+                    CREATE TABLE CUSTOMER(
+                            ID INTEGER,
+                            NAME VARCHAR(50),
+                            SURNAME VARCHAR(50),
+                            ADDRESS VARCHAR(300),
+                            TOTAL_ORDERS INTEGER DEFAULT 0,
+                            CONSTRAINT CONSTRAINT1
+                            FOREIGN KEY (ID) REFERENCES SITEUSER(ID)
+                            ON DELETE CASCADE);
+    ""","""               CREATE TABLE COMPANY(
+                        ID INTEGER,
+                        NAME VARCHAR(40),
+                        AVGDAY INTEGER,
+                        YEAR_FOUNDED INTEGER,
+                        TOTAL_ORDERS INTEGER DEFAULT 0,
+                        CONSTRAINT CONSTRAINT1
+                            FOREIGN KEY (ID) REFERENCES SITEUSER(ID)
+                            ON DELETE CASCADE);
+    ""","""             CREATE TABLE MYORDER(
+                        ORDER_ID SERIAL PRIMARY KEY,
+                        CUSTOMER_ID INTEGER,
+                        COMPANY_ID INTEGER,
+                        ORDER_DATE DATE NOT NULL DEFAULT CURRENT_DATE,
+                        ITEM VARCHAR(100),
+                        CONSTRAINT CONSTRAINT1
+                            FOREIGN KEY (CUSTOMER_ID) REFERENCES SITEUSER(ID)
+                            ON DELETE CASCADE,
+                        CONSTRAINT CONSTRAINT2
+                            FOREIGN KEY (COMPANY_ID) REFERENCES SITEUSER(ID)
+                            ON DELETE CASCADE);
+                        """
 ]
 
 
